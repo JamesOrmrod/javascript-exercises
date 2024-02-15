f62e94cb1c5b943a0e0b13c6046b63dccf6bcd2c
James Ormrod <88646794+JamesOrmrod@users.noreply.github.com>
2024-02-16 10:16:16 +1100

    Add completed exercises 3 and 4.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(origArray, ...removeArray) {
+    for (let i = 0; i < removeArray.length; i++) {
+        while (origArray.indexOf(removeArray[i]) >= 0) {
+            origArray.splice(origArray.indexOf(removeArray[i]), 1);
+        }
+    }
+    return origArray;
 };
 
 // Do not edit below this line
