168d742ae7a959bef9755be4fe6e69b9e70232fc
James Ormrod <88646794+JamesOrmrod@users.noreply.github.com>
2024-02-16 09:53:58 +1100

    Add completed exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    // create an empty string to house the concatentation
+    fullString = "";
+    
+    // can't repeat a negative number of times, so return ERROR
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    // loop through concatenating the string on the end
+    for (let i = 0; i < num; i++) {
+        fullString += string;
+    }
+    return fullString;
 };
 
 // Do not edit below this line
