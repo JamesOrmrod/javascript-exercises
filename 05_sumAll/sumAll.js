7854f9d4babce84e0f064ade7afb7dc451148aa0
James Ormrod <88646794+JamesOrmrod@users.noreply.github.com>
2024-02-16 11:43:05 +1100

    Add completed exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,43 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total;
 
+    // don't try to sum a range where one value is not a number
+    if (num1 < 0 || num2 < 0 || !(isNumber(num1)) || !(isNumber(num2))) {
+        return "ERROR";
+    }
+
+    let numStart;
+    let numEnd;
+
+    // find the smaller number and make it the lower limit
+    // if (num1 = num2) {
+    //     return num1;
+    // }
+    
+    if(num1 <= num2) {
+        numStart = +num1;
+        numEnd = +num2;
+    } else {
+        numStart = +num2;
+        numEnd = +num1;
+    }
+
+    // for (let i = numStart; i <= numEnd; i++) {
+    //     total += i;
+    // }
+
+    total = 0;
+    
+    for (; numStart <= numEnd; numStart++) {
+        total += numStart;
+    }
+
+    return total;
 };
+
+function isNumber(value) {
+    return typeof value === 'number';
+}
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // formula to convert fht to cel: x °F ≘ ((x − 32) × 5 / 9) °C
+  let result = (temp - 32) * 5 / 9;
+  result = result.toFixed(1);
+  return +result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // formula to convert fht to cel: x °C ≘ (x × 9 / 5 + 32) °F
+  let result = temp * 9 / 5 + 32;
+  result = result.toFixed(1);
+  return +result;
 };
 
 // Do not edit below this line
