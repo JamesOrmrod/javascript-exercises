2a280304967b140f4549d11323bf839288c485ee
James Ormrod <88646794+JamesOrmrod@users.noreply.github.com>
2024-02-23 15:05:10 +1100

    All exercises completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(person => person.age = calcAge(person));
+        
+    people.sort((a, b) => b.age - a.age);
+    
+    return people[0];
 };
+
+// const findTheOldest = function(people) {
+//     let oldestPerson = {};
+//     people.reduce((oldestPerson, person) => {
+//         if (oldestPerson === undefined) {
+//             oldestPerson = person;
+//         } else if (calcAge(person) > calcAge(oldestPerson)) {
+//             oldestPerson = person;
+//         }
+//     });
+//     return oldestPerson;
+// };
+
+
+function calcAge(person) {
+    if (person.yearOfDeath == undefined) {
+        return 2024 - person.yearOfBirth;
+    } else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
